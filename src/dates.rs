c49419e7afea0f45a9a5fa6f144f8a10c0adebd2
Michael Kreuzer <m-git@kreuzers.org>
2017-12-09 23:16:17 +0100

    implemented csv parsing and next cmd

---
+++ src/dates.rs
@@ -0,0 +1,62 @@
+use std::collections::{VecDeque, HashMap};
+use chrono::prelude::*;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct DateMgr {
+    dates : HashMap<TrashType, VecDeque<NaiveDate>>
+}
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+pub struct TrashType {
+    pub name : String
+}
+
+impl DateMgr {
+    pub fn new() -> DateMgr {
+        DateMgr { dates: HashMap::new() }
+    }
+
+    pub fn dates(&self) -> &HashMap<TrashType, VecDeque<NaiveDate>> {
+        &self.dates
+    }
+
+    pub fn append_file(&mut self, filename: &str) -> Result<(), Box<Error>> {
+        let f = File::open(filename)?;
+        let mut rdr = BufReader::new(f);
+
+        //Parse Header
+        let mut header = String::new();
+        rdr.read_line(&mut header)?;
+        let mut idx = Vec::new(); //Saves names with index in file
+        for s in header.split(';') {
+            let trash = TrashType{name : String::from(s.trim())};
+            self.dates.entry(trash.clone()).or_insert(VecDeque::new());
+            idx.push(trash);
+        }
+
+        //Parse dates
+        for l in rdr.lines().filter_map(|result| result.ok()) {
+            let split = l.split(";");
+            if let (4, _) = split.size_hint() {
+                continue;
+            }
+            let mut i = 0;
+            for s in split.map(|s| s.trim()) {
+                if !s.is_empty() {
+                    let date = NaiveDate::parse_from_str(s, "%d.%m.%Y")?;
+                    //Remove past dates, maybe move this to other fn
+                    if date > Local::now().naive_local().date() {
+                        self.dates.get_mut(&idx[i]).unwrap().push_back(date);
+                    }
+                }
+                i = i+1;
+            }
+        }
+
+        println!("{:?}", self);
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,45 @@ extern crate serde;
 extern crate serde_json;
 extern crate iron;
 extern crate router;
+extern crate chrono;
 
 use tgapi::SendMessage;
 use tgapi::types::*;
 use tgapi::TgApi;
+use dates::*;
 
 mod tgapi;
+mod dates;
 
 
 fn main() {
     //tgapi::run();
-    TgApi::new().unwrap().start_listen(handle_update);
+    let mut mgr = DateMgr::new();
+    mgr.append_file("termine.csv").unwrap();
+    TgApi::new().unwrap().start_listen(move |u| handle_update(u, &mgr));
 }
 
-fn handle_update(up : Update) {
+fn handle_update(up : Update, mgr : &DateMgr) {
     match up.message {
         Some(m) => {
-            let msg =SendMessage{chat_id : m.chat.id, text : &m.text.unwrap_or(String::from("empty message"))};
-            tgapi::TgApi::new().unwrap().send(msg).unwrap()
+            match m.text {
+                Some(ref t) if t == "/next" => {
+                    let msg =SendMessage{chat_id : m.chat.id, text : &get_next_dates(mgr)};
+                    tgapi::TgApi::new().unwrap().send(msg).unwrap()
+                }
+                _ => {
+                    let msg =SendMessage{chat_id : m.chat.id, text : &m.text.unwrap_or(String::from("empty message"))};
+                    tgapi::TgApi::new().unwrap().send(msg).unwrap()
+                }
+            }
         },
         None => println!("Empty update")
     }
+}
+
+fn get_next_dates(mgr : &DateMgr) -> String {
+    mgr.dates().iter()
+        .filter(|entry| entry.1.get(0).is_some())
+        .map(|(tonne, date)| format!("{} : {}", tonne.name, date.get(0).unwrap().format("%Y-%m-%d")))
+        .fold(String::new(), |mut string, item| {string.push_str(&item); string.push_str("\n"); string})
 }
