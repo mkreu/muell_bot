4ec24e5fdec891da516721e50bd088ef3f367252
Michael Kreuzer <michael.kreuzer@stud-mail.uni-wuerzburg.de>
2018-03-26 08:02:31 +0200

    modified ci_update

--- ci_update.sh
+++ ci_update.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 [ -e deploy.log ] && rm deploy.log
-#git pull && \
-#go build && \
-tmux new-session -s muell_bot \; detach > /dev/zero
-tmux respawn-window -k -t muell_bot "cd /home/michael/rustPlayground/muell_bot; ./deploy.sh ; ./target/debug/muell_bot" > /dev/zero
-#touch deploy.log
-while [ ! -e deploy.log ]
-do
-	sleep 0.2
-done
+cd /home/michael/rustPlayground/muell_bot; ./deploy.sh > /dev/zero
 cat deploy.log
+supervisorctl restart muell_bot
 
