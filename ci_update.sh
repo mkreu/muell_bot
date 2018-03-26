400bee5043c827c6a157f297dcb73fd9631c47d1
Michael Kreuzer <michael.kreuzer@stud-mail.uni-wuerzburg.de>
2018-03-26 08:14:23 +0200

    fixed path in ci

--- ci_update.sh
+++ ci_update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ -e deploy.log ] && rm deploy.log
-cd /home/michael/rustPlayground/muell_bot; ./deploy.sh > /dev/zero
+cd /home/walnut/muell_bot; ./deploy.sh > /dev/zero
 cat deploy.log
 supervisorctl restart muell_bot
 
