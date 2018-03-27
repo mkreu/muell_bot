f6ca73c1a957671ee500d9f92f6c7428528ed211
Michael Kreuzer <michael.kreuzer@stud-mail.uni-wuerzburg.de>
2018-03-27 09:55:29 +0200

    ci stuff

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "deploying at `date` ..." > .deploy.log
+echo "in dir $PWD"
 LASTHEAD="`cat .git/refs/heads/master`"
 LOGRANGE=""
 [ "$LASTHEAD" != "" ] && LOGRANGE="${LASTHEAD}..HEAD"
 git pull >> .deploy.log
 git shortlog "$LOGRANGE" >> .deploy.log
 echo "building ..." >> .deploy.log
-{ .cargo/bin/cargo build && echo "build successful." || echo "ERROR: build failed!" ; } >> .deploy.log 2>&1
+{ ~/.cargo/bin/cargo build && echo "build successful." || echo "ERROR: build failed!" ; } >> .deploy.log 2>&1
 echo "finished." >> .deploy.log
 
 mv .deploy.log deploy.log
