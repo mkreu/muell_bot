6111001be781d628756afab4debcd408ef2fe5e8
Michael Kreuzer <michael.kreuzer@stud-mail.uni-wuerzburg.de>
2018-04-18 09:47:41 +0200

    cargo release build argument

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ LOGRANGE=""
 git pull >> .deploy.log
 git shortlog "$LOGRANGE" >> .deploy.log
 echo "building ..." >> .deploy.log
-{ ~/.cargo/bin/cargo build && echo "build successful." || echo "ERROR: build failed!" ; } >> .deploy.log 2>&1
+{ ~/.cargo/bin/cargo build --release && echo "build successful." || echo "ERROR: build failed!" ; } >> .deploy.log 2>&1
 echo "finished." >> .deploy.log
 
 mv .deploy.log deploy.log
