f813d48958e49007d0157dba8262fee7d2f060ff
Michael Kreuzer <m-git@kreuzers.org>
2024-12-26 16:20:03 +0100

    replaced token

--- src/tgapi/mod.rs
+++ src/tgapi/mod.rs
@@ -1,7 +1,6 @@
 use serde::Deserialize;
 use std::fs::File;
 use std::io;
-use std::net::SocketAddr;
 
 pub mod receive;
 pub mod send;
@@ -10,8 +9,6 @@ pub mod types;
 #[derive(Deserialize)]
 pub struct ApiConf {
     pub token: String,
-    pub webhook_addr: SocketAddr,
-    pub webhook_path: String,
 }
 
 pub fn read_api_conf(filename: &str) -> io::Result<ApiConf> {
